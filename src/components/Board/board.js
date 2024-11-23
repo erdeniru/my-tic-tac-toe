2190cf4c14b0a9d0f14ca87f569caeb5e0ce5435
erdeni <erdeni@bk.ru>
2024-11-23 11:15:46 +0800

    Компонент Board изменен на классовый

--- src/components/Board/board.js
+++ src/components/Board/board.js
@@ -1,45 +1,73 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { selectBoard, selectStatus, selectWinPattern } from '../../store/selectors';
+import { connect } from 'react-redux';
 import { setBoardCellAction } from '../../store/action-creators';
 import { STATUS, PLAYER } from '../../constants';
 import styles from './board.module.css';
+import { Component } from 'react';
 
-export const Board = () => {
-    const status = useSelector(selectStatus);
-    const board = useSelector(selectBoard);
-    const winPattern = useSelector(selectWinPattern);
+class BoardContainer extends Component {
+    constructor(props) {
+        super(props);
 
-    const dispatch = useDispatch();
+        this.state = {
+            isGameOver: STATUS.TURN,
+        };
+    }
 
-    const isGameOver = status === STATUS.WIN || status === STATUS.DRAW;
-
-    const onCellClick = (indexCell) => {
-        dispatch(setBoardCellAction(indexCell));
+    updatedStatus = (status) => {
+        this.setState({
+            isGameOver: status === STATUS.WIN || status === STATUS.DRAW,
+        });
     };
 
-    return (
-        <div
-            className={
-                styles.content + (isGameOver ? ' ' + styles.content_disabled : '') + ''
-            }
-        >
-            {board.map((value, index) => (
-                <div
-                    key={index}
-                    className={
-                        // prettier-ignore
-                        styles.cell + ' ' + styles.bg__cell +
-                        (value === PLAYER.NOUGHT ? ' ' + styles.bg__cell_o : '') +
-                        (value === PLAYER.CROSS ? ' ' + styles.bg__cell_x : '') +
-                        (winPattern.indexOf(index) > -1 ? ' ' + styles.bg__cell_win : '') +
-                        (value !== PLAYER.NONE ? ' ' + styles.cell_noHover : '') +
-                        ''
-                    }
-                    {...(value === PLAYER.NONE
-                        ? { onClick: () => onCellClick(index) }
-                        : {})}
-                ></div>
-            ))}
-        </div>
-    );
-};
+    componentDidMount() {
+        this.updatedStatus(this.props.status);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.status === prevProps.status) return;
+
+        this.updatedStatus(this.props.status);
+    }
+
+    render() {
+        return (
+            <div
+                className={
+                    styles.content +
+                    (this.state.isGameOver ? ' ' + styles.content_disabled : '') +
+                    ''
+                }
+            >
+                {this.props.board.map((value, index) => (
+                    <div
+                        key={index}
+                        className={
+                            // prettier-ignore
+                            styles.cell + ' ' + styles.bg__cell +
+                            (value === PLAYER.NOUGHT ? ' ' + styles.bg__cell_o : '') +
+                            (value === PLAYER.CROSS ? ' ' + styles.bg__cell_x : '') +
+                            (this.props.winPattern.indexOf(index) > -1 ? ' ' + styles.bg__cell_win : '') +
+                            (value !== PLAYER.NONE ? ' ' + styles.cell_noHover : '') +
+                            ''
+                        }
+                        {...(value === PLAYER.NONE
+                            ? { onClick: () => this.props.onCellClick(index) }
+                            : {})}
+                    ></div>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    status: state.status,
+    board: state.board,
+    winPattern: state.winPattern,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    onCellClick: (indexCell) => dispatch(setBoardCellAction(indexCell)),
+});
+
+export const Board = connect(mapStateToProps, mapDispatchToProps)(BoardContainer);
