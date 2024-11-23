8f15f8f46612a3d11f9df2eb0110f129c56076c9
erdeni <erdeni@bk.ru>
2024-11-23 10:48:06 +0800

    Компонент Information изменен на классовый

--- src/components/Information/information.js
+++ src/components/Information/information.js
@@ -1,32 +1,63 @@
-import { useSelector } from 'react-redux';
-import { selectCurrentPlayer, selectStatus } from '../../store/selectors';
+import { connect } from 'react-redux';
 import { Player } from '../../components';
 import { STATUS } from '../../constants';
 import { MESSAGE_TURN, MESSAGE_CONGRATULATION, MESSAGE_DRAW } from '../../constants';
 import styles from './information.module.css';
+import { Component } from 'react';
 
-export const Information = () => {
-    const currentPlayer = useSelector(selectCurrentPlayer);
-    const status = useSelector(selectStatus);
+class InformationContainer extends Component {
+    constructor(props) {
+        super(props);
 
-    let message = '';
-    let classInformation = '';
-
-    if (status === STATUS.TURN) {
-        message = MESSAGE_TURN;
-        classInformation = styles.content__message;
-    } else if (status === STATUS.WIN) {
-        message = MESSAGE_CONGRATULATION;
-        classInformation = styles.content__congratulation;
-    } else if (status === STATUS.DRAW) {
-        message = MESSAGE_DRAW;
-        classInformation = styles.content__draw;
+        this.state = {
+            message: '',
+            classInformation: '',
+        };
     }
 
-    return (
-        <div className={styles.content}>
-            <div className={classInformation}>{message}</div>
-            <Player player={currentPlayer} />
-        </div>
-    );
-};
+    updatedStatus = (status) => {
+        console.log(status);
+        if (status === STATUS.TURN) {
+            this.setState({
+                message: MESSAGE_TURN,
+                classInformation: styles.content__message,
+            });
+        } else if (status === STATUS.WIN) {
+            this.setState({
+                message: MESSAGE_CONGRATULATION,
+                classInformation: styles.content__congratulation,
+            });
+        } else if (status === STATUS.DRAW) {
+            this.setState({
+                message: MESSAGE_DRAW,
+                classInformation: styles.content__draw,
+            });
+        }
+    };
+
+    componentDidMount() {
+        this.updatedStatus(this.props.status);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.status === prevProps.status) return;
+
+        this.updatedStatus(this.props.status);
+    }
+
+    render() {
+        return (
+            <div className={styles.content}>
+                <div className={this.state.classInformation}>{this.state.message}</div>
+                <Player player={this.props.currentPlayer} />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    currentPlayer: state.currentPlayer,
+    status: state.status,
+});
+
+export const Information = connect(mapStateToProps)(InformationContainer);
